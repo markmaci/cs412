3ac6502c1eebd307afff5b0605eac73559a365f1
markmaci <markmaci71@gmail.com>
2024-09-21 17:37:59 -0400

    remove duplicated assignment3 files

--- quotes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.quote, name='quote'),
-    path('quote/', views.quote, name='quote'),
-    path('show_all/', views.show_all, name='show_all'),
-    path('about/', views.about, name='about'),
-]
