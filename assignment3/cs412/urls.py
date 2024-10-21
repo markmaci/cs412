800946a08a68d207e9510ae76efaa91c589e44c5
markmaci <markmaci71@gmail.com>
2024-10-21 18:09:05 -0400

    modify urls to serve media images

--- assignment3/cs412/urls.py
+++ assignment3/cs412/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('mini_fb/', include('mini_fb.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
