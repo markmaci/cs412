3c91e381c85de3311975c0c50ca91d245b28f867
markmaci <markmaci71@gmail.com>
2024-12-09 14:03:00 -0500

    build.sh

---
+++ assignment3/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Updating pip..."
+python3.9 -m pip install --upgrade pip
+
+echo "Installing project dependencies..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Making migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"
