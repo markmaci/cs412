c0bd925a167c20b499e9d267c6aeb0a52e0afaec
markmaci <markmaci71@gmail.com>
2024-12-09 14:30:45 -0500

    staticfiles

--- assignment3/cs412/settings.py
+++ assignment3/cs412/settings.py
@@ -112,9 +112,9 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
