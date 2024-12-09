1bdd04135605b06f6b4d47ee8281fc563cf221fc
markmaci <markmaci71@gmail.com>
2024-12-09 14:23:27 -0500

    fix static root

--- assignment3/cs412/settings.py
+++ assignment3/cs412/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
