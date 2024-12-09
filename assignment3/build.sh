dfdd1ae329b7e1c537140b2a92e4ccddc8edc3cc
markmaci <markmaci71@gmail.com>
2024-12-09 14:04:42 -0500

    upgrade pip

--- assignment3/build.sh
+++ assignment3/build.sh
@@ -3,7 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Updating pip..."
+echo "Ensuring pip is installed..."
+python3.9 -m ensurepip --upgrade
+
+echo "Upgrading pip..."
 python3.9 -m pip install --upgrade pip
 
 echo "Installing project dependencies..."
